81e765d6bb9189184a158a647c29380bef9e30de
tanya598 <obrysnik.tanya@gmail.com>
2023-05-25 15:16:53 +0300

    Переконайтеся, що у відповідній папці (recipe/templates/recipe/) є шаблони main.html і category_detail.html. Також не забудьте оновити файл urls.py, щоб включити шаблони URL-адрес для цих переглядів.
    Цей файл містить функції перегляду для програми рецептів.
    Він імпортує необхідні модулі та класи (render, Recipe, Category, sample) з Django та Python.
    Функція main() view відповідає за перегляд головної сторінки.
    Він вибирає 10 випадкових рецептів із бази даних за допомогою функції вибірки з модуля випадкових.
    Вибрані рецепти передаються до шаблону main.html як змінна recipes у контекстному словнику.
    Функція render() використовується для відтворення шаблону з наданим контекстом і повернення HTTP-відповіді.
    Функція перегляду category_detail() відповідає за відображення рецептів певної категорії.
    Для визначення категорії потрібен параметр category_id.
    Він отримує об’єкт категорії з бази даних за допомогою наданого category_id.
    Він отримує всі рецепти, пов’язані з категорією, використовуючи зворотний зв’язок рецептів.
    Об’єкти категорії та рецепти передаються до шаблону category_detail.html у контекстному словнику.
    Функція render() використовується для відтворення шаблону з наданим контекстом і повернення HTTP-відповіді.

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,20 @@
 
+from django.shortcuts import render
+from .models import Recipe, Category
+from random import sample
+
+def main(request):
+    random_recipes = sample(list(Recipe.objects.all()), 10)
+    context = {
+        'recipes': random_recipes
+    }
+    return render(request, 'main.html', context)
+
+def category_detail(request, category_id):
+    category = Category.objects.get(id=category_id)
+    recipes = category.recipes.all()
+    context = {
+        'category': category,
+        'recipes': recipes
+    }
+    return render(request, 'category_detail.html', context)
